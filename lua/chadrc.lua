2701f17f3f4a4c378f9ef40392ed559de3e39c03
FranChesK0 <khl_doss@mail.ru>
2024-12-01 18:43:31 +0800

    update ui

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,26 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
-
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "doomchad",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- Italic comments
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+}
+
+-- Remove 'Toggle Theme' and 'Exit' buttons
+M.ui = {
+  tabufline = {
+    order = { "treeOffset", "buffers", "tabs" },
+  },
+}
+
+-- Enable NvDash on startup
+M.nvdash = {
+  load_on_startup = true,
 }
 
 return M
