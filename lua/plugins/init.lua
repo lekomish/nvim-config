ef076b6f76c0333c6b2037d0e2a551e586d6e783
FranChesK0 <khl_doss@mail.ru>
2025-02-20 15:13:59 +0800

    add telescope functionality for lazygit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -87,8 +87,12 @@ return {
       "LazyGitFilterCurrentFile",
     },
     dependencies = {
+      "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim",
     },
+    config = function()
+      require("telescope").load_extension "lazygit"
+    end,
   },
 
   -- Asynchronous IO
